7cc0c7becaee6370a3fc9b408163d3ddf200f935
brysach <bryan.estradach@gmail.com>
2025-11-15 10:21:01 -0800

    Created Gemini-API file

---
+++ gemini-api.py
@@ -0,0 +1,16 @@
+from google import genai
+import os
+
+# print("GEMINI_API_KEY =", repr(os.getenv("GEMINI_API_KEY")))
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+uploaded_file1 = client.files.upload(file = "pizza.jpg")
+uploaded_file2 = client.files.upload(file = "King_Arthur.png")
+
+response = client.models.generate_content(
+    model = "gemini-2.5-flash",
+    contents = ["Can you make up a short story based off these two images", uploaded_file1, uploaded_file2]
+)
+
+print(response.text)
